559d0055aeab38347119aee3e6bcec08f0688f3a
Craig O'Donnell <cbodonnell@comcast.net>
2020-05-25 10:04:00 -0400

    cors

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,25 +1,20 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:4200"}})
 
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify(['data'])
-
-
-@app.route('/api/data', methods=['POST'])
-def set_data():
+@app.route('/api/path', methods=['POST'])
+def find_path():
     body = request.get_json()
+    print(body)
     origin = int(request.args.get('origin', None))
     destination = int(request.args.get('destination', None))
-    response = {
-        'origin': origin,
-        'destination': destination,
-        'body': body
-    }
+    #  Find path
+    response = [100, 200, 300]
     return jsonify(response)
 
 
