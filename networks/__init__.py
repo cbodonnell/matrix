91b65d8cdf53dd2b754fa6b821c63b65040e4ab9
cbodonnell <cheebzmusic@gmail.com>
2020-05-31 01:21:24 -0400

    clean up

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import numpy as np
 from networks.models import Network
 
 
@@ -15,8 +14,6 @@ def find_path():
     origin_node = int(request.args.get('origin', None))
     destination_node = int(request.args.get('destination', None))
     # TODO: Refactor to network constructor method
-    # inbound = {int(k): v['inbound'] for k, v in resource['nodes'].items()}
-    # outbound = {int(k): v['outbound'] for k, v in resource['nodes'].items()}
     nodes = {int(k): v for k, v in resource['nodes'].items()}
     links = {int(k): v for k, v in resource['links'].items()}
     for link in links:
@@ -31,11 +28,6 @@ def find_path():
             links[link]['start']: links[link]['end'],
             links[link]['end']: links[link]['start']
         }
-    # num_links = len(resource['links'])
-    # turn_matrix = np.full((num_links, num_links), False)
-    # for origin in resource['links']:
-    #     for destination in resource['links'][origin]['destinations']:
-    #         turn_matrix[links[int(origin)]][links[destination]] = True
     network = Network(nodes, links)
     path = network.find_path(origin_node, destination_node)
     return jsonify(path)
