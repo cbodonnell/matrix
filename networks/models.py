5c1c07a3c2860a6bde6ec96849d054b5a1f26b06
cbodonnell <cheebzmusic@gmail.com>
2020-06-14 17:48:16 -0400

    handle no path conditions

--- networks/models.py
+++ networks/models.py
@@ -48,27 +48,30 @@ class Network:
                     'cost': math.inf,
                     'from': [origin_node]
                 }
-        while not queue[destination_node]['complete']:
-            cheapest_node = None
-            lowest_cost = math.inf
-            for node in queue:
-                if not queue[node]['complete'] \
-                        and queue[node]['cost'] < lowest_cost:
-                    cheapest_node = node
-                    lowest_cost = queue[node]['cost']
-            current_node = cheapest_node
-            queue[current_node]['complete'] = True
-            for link in self.nodes[current_node]['outbound']:
-                new_cost = queue[current_node]['cost'] + self.links[link]['costs'][current_node]
-                if not queue[self.links[link]['opposite'][current_node]]['complete'] \
-                        and queue[self.links[link]['opposite'][current_node]]['cost'] >= new_cost:
-                    if queue[self.links[link]['opposite'][current_node]]['cost'] == new_cost:
-                        queue[self.links[link]['opposite'][current_node]]['from'].append(current_node)
-                    else:
-                        queue[self.links[link]['opposite'][current_node]]['from'] = [current_node]
-                        queue[self.links[link]['opposite'][current_node]]['cost'] = new_cost
-        # cost = queue[destination_node]['cost']
-        path = [destination_node]
-        while path[0] != origin_node:
-            path.insert(0, random.choice(queue[path[0]]['from']))
-        return path
+        try:
+            while not queue[destination_node]['complete']:
+                cheapest_node = None
+                lowest_cost = math.inf
+                for node in queue:
+                    if not queue[node]['complete'] \
+                            and queue[node]['cost'] < lowest_cost:
+                        cheapest_node = node
+                        lowest_cost = queue[node]['cost']
+                current_node = cheapest_node
+                queue[current_node]['complete'] = True
+                for link in self.nodes[current_node]['outbound']:
+                    new_cost = queue[current_node]['cost'] + self.links[link]['costs'][current_node]
+                    if not queue[self.links[link]['opposite'][current_node]]['complete'] \
+                            and queue[self.links[link]['opposite'][current_node]]['cost'] >= new_cost:
+                        if queue[self.links[link]['opposite'][current_node]]['cost'] == new_cost:
+                            queue[self.links[link]['opposite'][current_node]]['from'].append(current_node)
+                        else:
+                            queue[self.links[link]['opposite'][current_node]]['from'] = [current_node]
+                            queue[self.links[link]['opposite'][current_node]]['cost'] = new_cost
+            # cost = queue[destination_node]['cost']
+            path = [destination_node]
+            while path[0] != origin_node:
+                path.insert(0, random.choice(queue[path[0]]['from']))
+            return path
+        except Exception as e:
+            return None
