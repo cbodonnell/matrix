d517942fa08d71f4fd719b225d5eda788c1c7be6
cbodonnell <cheebzmusic@gmail.com>
2020-10-08 21:57:58 -0400

    another cors try

--- networks/__init__.py
+++ networks/__init__.py
@@ -14,7 +14,7 @@ else:
 print('ENV is set to: %s' % app.config["ENV"])
 
 # cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:4200"}})
-cors = CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 app.register_blueprint(path_blueprint)
